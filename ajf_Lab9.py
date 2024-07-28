d24507b5b8461a694750baef58cbca7986ced76c
Flegerta <flegerta@ufl.edu>
2024-07-27 22:40:00 -0400

    Joined the two

--- ajf_Lab9.py
+++ ajf_Lab9.py
@@ -22,3 +22,8 @@ def encode(password):
         if x == "9":
             final.append("2")
     return "".join(final)
+
+
+def decode(password):
+    decoded_password = ''.join(str((int(char) - 3) % 10) for char in password)
+    return decoded_password
